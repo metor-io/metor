d065be3e0229032aa953c7528a312c1266e76c92
Sascha Wise <me@saschawise.com>
2025-08-03 11:35:17 -0700

    fix(editor): remove python stuff

--- apps/elodin/src/cli/editor.rs
+++ apps/elodin/src/cli/editor.rs
@@ -6,146 +6,24 @@ use miette::{IntoDiagnostic, miette};
 use std::io::{Read, Seek, Write};
 use std::net::{Ipv6Addr, SocketAddr};
 use std::path::PathBuf;
-use std::thread::JoinHandle;
 use stellarator::util::CancelToken;
 use tokio::runtime::Runtime;
 
 use super::Cli;
 
-const DEFAULT_SIM: Simulator = Simulator::None;
-
-#[derive(clap::Args, Clone, Default)]
-pub struct Args {
-    #[clap(name = "addr/path", default_value_t = DEFAULT_SIM)]
-    sim: Simulator,
-}
-
-#[derive(Clone)]
-enum Simulator {
-    None,
-    Addr(SocketAddr),
-    File(PathBuf),
-    ReplayDir(PathBuf),
-}
-
 #[derive(Resource)]
 struct WindowStateFile(std::fs::File);
 
-impl Default for Simulator {
-    fn default() -> Self {
-        DEFAULT_SIM
-    }
-}
-
-impl fmt::Display for Simulator {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::None => write!(f, ""),
-            Self::Addr(addr) => write!(f, "{}", addr),
-            Self::File(path) => write!(f, "{}", path.display()),
-            Self::ReplayDir(path) => write!(f, "{}", path.display()),
-        }
-    }
-}
-
-impl std::str::FromStr for Simulator {
-    type Err = miette::Error;
-    fn from_str(s: &str) -> miette::Result<Self> {
-        if s.is_empty() {
-            return Ok(Self::None);
-        }
-        if let Ok(addr) = s.parse() {
-            Ok(Self::Addr(addr))
-        } else {
-            let path = PathBuf::from(s);
-            if path.is_dir() {
-                Ok(Self::ReplayDir(path))
-            } else {
-                Ok(Self::File(path))
-            }
-        }
-    }
-}
-
 impl Cli {
-    #[cfg(not(target_os = "windows"))]
-    pub fn run_sim(
-        &self,
-        args: &Args,
-        rt: Runtime,
-        cancel_token: CancelToken,
-    ) -> miette::Result<JoinHandle<miette::Result<()>>> {
-        let sim = args.sim.clone();
-        let dirs = self.dirs().into_diagnostic()?;
-        let cache_dir = dirs.cache_dir().to_owned();
-        let thread = std::thread::spawn(move || {
-            rt.block_on(async move {
-                let ctrl_c_cancel_token = cancel_token.clone();
-                tokio::spawn(async move {
-                    let _drop = ctrl_c_cancel_token.drop_guard(); // binding needs to be named to ensure drop is called at end of scope
-                    tokio::signal::ctrl_c().await.unwrap();
-                    info!("Received Ctrl-C, shutting down");
-                });
-                if let Simulator::File(path) = &sim {
-                    let res = elodin_editor::run::run_recipe(
-                        cache_dir,
-                        path.clone(),
-                        cancel_token.clone(),
-                    )
-                    .await;
-                    cancel_token.cancel();
-                    res
-                } else {
-                    Ok(())
-                }
-            })
-        });
-        Ok(thread)
-    }
-
-    #[cfg(target_os = "windows")]
-    pub fn run_sim(
-        &self,
-        _args: &Args,
-        rt: Runtime,
-        cancel_token: CancelToken,
-    ) -> miette::Result<JoinHandle<miette::Result<()>>> {
-        Ok(std::thread::spawn(move || {
-            rt.block_on(async move {
-                tokio::spawn(async move {
-                    let _drop = cancel_token.drop_guard(); // binding needs to be named to ensure drop is called at end of scope
-                    tokio::signal::ctrl_c().await
-                });
-                Ok(())
-            })
-        }))
-    }
-
-    pub fn editor(self, args: Args, rt: Runtime) -> miette::Result<()> {
+    pub fn editor(self, rt: Runtime) -> miette::Result<()> {
         let cancel_token = CancelToken::new();
-        let thread = self.run_sim(&args, rt, cancel_token.clone())?;
         let mut app = self.editor_app()?;
-        match args.sim {
-            Simulator::None => {
-                app.add_plugins(impeller2_bevy::TcpImpellerPlugin::new(None));
-            }
-            Simulator::Addr(addr) => {
-                app.add_plugins(impeller2_bevy::TcpImpellerPlugin::new(Some(addr)));
-            }
-            Simulator::File(_) => {
-                app.add_plugins(impeller2_bevy::TcpImpellerPlugin::new(Some(
-                    SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), 2240),
-                )));
-            }
-            Simulator::ReplayDir(_) => {
-                // TODO
-            }
-        };
+        app.add_plugins(impeller2_bevy::TcpImpellerPlugin::new(self.addr));
         app.insert_resource(BevyCancelToken(cancel_token.clone()))
             .add_systems(Update, check_cancel_token);
         app.run();
         cancel_token.cancel();
-        thread.join().map_err(|_| miette!("join error"))?
+        Ok(())
     }
 
     pub fn editor_app(&self) -> miette::Result<App> {
--- apps/elodin/src/cli/mod.rs
+++ apps/elodin/src/cli/mod.rs
@@ -2,32 +2,16 @@ use clap::{Parser, Subcommand};
 use miette::Context;
 use miette::IntoDiagnostic;
 use miette::miette;
+use std::net::SocketAddr;
 use stellarator::util::CancelToken;
 use tracing_subscriber::EnvFilter;
 
-mod create;
 mod editor;
 
 #[derive(Parser, Clone)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    #[arg(short, long, default_value = "https://app.elodin.systems")]
-    url: String,
-    #[command(subcommand)]
-    command: Option<Commands>,
-    #[arg(long, hide = true)]
-    markdown_help: bool,
-}
-
-#[derive(Subcommand, Clone)]
-enum Commands {
-    /// Launch the Elodin editor (default)
-    Editor(editor::Args),
-    /// Run an Elodin simulaton in headless mode
-    #[cfg(not(target_os = "windows"))]
-    Run(editor::Args),
-    /// Create template
-    Create(create::Args),
+    addr: Option<SocketAddr>,
 }
 
 impl Cli {
@@ -36,11 +20,6 @@ impl Cli {
     }
 
     pub fn run(self) -> miette::Result<()> {
-        if self.markdown_help {
-            clap_markdown::print_help_markdown::<Cli>();
-            std::process::exit(0);
-        }
-
         let filter = if std::env::var("RUST_LOG").is_ok() {
             EnvFilter::builder().from_env_lossy()
         } else {
@@ -61,53 +40,11 @@ impl Cli {
             .build()
             .expect("tokio runtime failed to start");
 
-        if let Err(err) = self.first_launch() {
-            eprintln!("Error: {:#}", err);
-            std::process::exit(1);
-        }
-
-        match &self.command {
-            Some(Commands::Editor(args)) => self.clone().editor(args.clone(), rt),
-            #[cfg(not(target_os = "windows"))]
-            Some(Commands::Run(args)) => self
-                .run_sim(args, rt, CancelToken::new())?
-                .join()
-                .map_err(|_| miette!("join error"))?,
-            Some(Commands::Create(args)) => self.create_template(args).into_diagnostic(),
-            None => self.clone().editor(editor::Args::default(), rt),
-        }
-    }
-
-    fn first_launch(&self) -> miette::Result<()> {
-        let dirs = self.dirs().into_diagnostic()?;
-        let data_dir = dirs.data_dir();
-        let is_first_launch = !data_dir.exists();
-        std::fs::create_dir_all(data_dir)
-            .into_diagnostic()
-            .context("failed to create data directory")?;
-
-        if is_first_launch {
-            println!("This is your first use of the Elodin CLI!\n");
-
-            println!(
-                "Ensure the Elodin Python SDK is installed in your preferred Python virtual environment:"
-            );
-            println!("    pip install -U elodin\n");
-
-            println!("Check out our docs (at https://docs.elodin.systems) for more information.");
-            std::process::exit(0);
-        }
-
-        Ok(())
-    }
-
-    fn is_dev(&self) -> bool {
-        self.url.ends_with("elodin.dev") || self.url.contains("localhost")
+        self.editor(rt)
     }
 
     fn dirs(&self) -> Result<directories::ProjectDirs, std::io::Error> {
-        let app_name = if self.is_dev() { "cli-dev" } else { "cli" };
-        directories::ProjectDirs::from("systems", "elodin", app_name).ok_or(std::io::Error::new(
+        directories::ProjectDirs::from("systems", "elodin", "console").ok_or(std::io::Error::new(
             std::io::ErrorKind::NotFound,
             "failed to get data directory",
         ))
